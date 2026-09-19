c6e02017e812890d2069ddb2556fe7bfeb90dbe1
Langl3y <hieuhovan954@gmail.com>
2026-09-19 17:53:13 +0700

    Rename model file's name to match API's

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,4 +1,4 @@
 from .base import db, row_to_dict, get_primary_key, new_session
 from .user import *
-from .breed import Breed, BreedImage, Species
+from .dog import Breed, BreedImage, Species
 from .system import File
