09bc481a94d4025c32781d56da361d7380551a01
Langl3y <hieuhovan954@gmail.com>
2026-09-19 18:31:25 +0700

    Add EnumField to extra fields for querying APIs

--- app/api/common/__init__.py
+++ app/api/common/__init__.py
@@ -1,6 +1,7 @@
 from .base import Api, Resource, Namespace, JsonEncoder
 from .decorators import respond_with_code, require_login
-from .extra_fields import Object, DecimalType, EnumType, CustomString, PageField, LimitField
+from .extra_fields import (Object, DecimalType, EnumType, EnumField,
+                           CustomString, PageField, LimitField)
 from .request import (get_request_ip, get_request_platform,
                       get_request_language, get_request_user_agent,
                       get_request_data, get_request_info, get_request_user)
